8a3bc45c0f68d14f828a89aab4214f4f5281ce26
Galina Hedda <gahe4951@bht-berlin.de>
2025-10-19 16:16:56 +0200

    Add pom.xml and source files

---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,15 @@
+package com.mycompany.app;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
